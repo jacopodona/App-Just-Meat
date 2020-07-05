5d442e129740cd01bb74d94e03eaf4a3431c4882
davide <davide.farina.1998@gmail.com>
2020-07-05 16:28:44 +0200

    restart project

---
+++ JustMeat/app/src/main/java/com/example/justmeat/utilities/Constants.java
@@ -0,0 +1,5 @@
+package com.example.justmeat.utilities;
+
+public final class Constants {
+    public static final String CONNECTION_STRING = "http://192.168.1.70:8080";
+}
---
+++ JustMeat/app/src/main/java/com/example/justmeat/utilities/HttpJsonRequest.java
@@ -0,0 +1,87 @@
+package com.example.justmeat.utilities;
+
+import android.content.Context;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpJsonRequest {
+    private JsonObjectRequest request;
+    private RequestQueue queue;
+
+    /***
+     *
+     * @param context context is always "this"
+     * @param partialUrl the api code, for example "/test"
+     * @param method the api method, for example Request.Method.GET
+     * @param body optional json request body
+     * @param onResponse what to do when server has responded
+     * @param onError what to do then an error occurs
+     */
+    public HttpJsonRequest(Context context, String partialUrl, int method, JSONObject body, Response.Listener<JSONObject> onResponse, Response.ErrorListener onError) {
+        request = new JsonObjectRequest(method, Constants.CONNECTION_STRING + partialUrl, body, onResponse, onError) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("Content-Type", "application/json");
+
+                // Get bearer token from storage if exists
+                String token = "null";
+                params.put("Authorization", ("Bearer " + token));
+                return params;
+            }
+        };
+        queue = Volley.newRequestQueue(context);
+    }
+
+    /***
+     *
+     * @param context context is always "this"
+     * @param partialUrl the api code, for example "/test"
+     * @param method the api method, for example Request.Method.GET
+     * @param onResponse what to do when server has responded
+     * @param onError what to do then an error occurs
+     */
+    public HttpJsonRequest(Context context, String partialUrl, int method, Response.Listener<JSONObject> onResponse, Response.ErrorListener onError) {
+        this(context, partialUrl, method, null, onResponse, onError);
+    }
+
+    public void run() {
+        queue.add(request);
+    }
+}
+
+
+
+/*  Usage example
+
+try {
+    JSONObject body = new JSONObject();
+    body.put("test", "Hello");
+
+    new HttpJsonRequest(this, "/test", Request.Method.POST, body,
+        new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // Do something within context
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // An error occurred
+            }
+        }
+    ).run();
+} catch(JSONException ex) {
+   return;
+}
+
+*/
