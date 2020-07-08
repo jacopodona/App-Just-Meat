3c9a062d997fa6603150f605974263ffde6614f9
jacopodona <49444372+jacopodona@users.noreply.github.com>
2020-07-08 12:55:06 +0200

    aggiunta searchBar e seekBar

--- JustMeat/app/src/main/java/com/example/justmeat/homepage/TrovaSupermercatiFragment.java
+++ JustMeat/app/src/main/java/com/example/justmeat/homepage/TrovaSupermercatiFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,8 +24,32 @@ import java.util.List;
 public class TrovaSupermercatiFragment extends Fragment {
     @Nullable
     @Override
+
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_trova_supermercati,container,false);
+
+
+        SeekBar seekbar= (SeekBar) view.findViewById(R.id.homepage_seekbar_distanza);
+        final TextView textView= (TextView) view.findViewById(R.id.homepage_textview_distanza);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(""+progress+" km");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
 
         List lista= new LinkedList();
         Supermercato m= new Supermercato("Aldi","Via Roma 12b");
